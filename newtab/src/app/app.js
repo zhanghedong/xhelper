43ad6374e1da61f1608cee9b3a5de8fd25e62640
1625 <zhanghedong@gmail.com>
2014-02-17 00:22:09 +0800

    test

--- newtab/src/app/app.js
+++ newtab/src/app/app.js
@@ -1,18 +1,22 @@
+var ntp = {};
 angular.module('app', [
     'ngRoute',
     'templates.app',
+    'resources.sites',
     'home',
     'bgDirectives'
 ]);
-
-angular.module('app').config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+//app.config(['$compileProvider', function($compileProvider) {
+//    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|file|tel):/);
+//}]);
+angular.module('app').config(['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider', function ($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
         //$locationProvider.html5Mode(true);
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob|chrome-extension):|data:image\//);
         $routeProvider.when('/home', {
             templateUrl: 'home/home.tpl.html',
             controller: 'homeCtrl'
         }).otherwise({redirectTo: '/home'});
-
-
     }]).run(function ($route, $http, $templateCache) {
         angular.forEach($route.routes, function (r) {
             if (r.templateUrl) {
--- newtab/src/app/home/home.js
+++ newtab/src/app/home/home.js
@@ -2,17 +2,63 @@
  * Created by zhanghd on 13-10-11 下午4:39
  * Copyright 2013 17173, Inc. All rights reserved.
  */
-angular.module('home', [], ['$routeProvider', function ($routeProvider) {
+angular.module('home', ['resources.sites'], ['$routeProvider', function ($routeProvider) {
 
 }]);
-angular.module('home', ['config']).controller('homeCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
-        chrome.topSites.get(function (d) {
-            $timeout(function(){
-                d.length = 10;
-                $scope.items = d;
-            })
+angular.module('home', ['config']).controller('homeCtrl', ['$scope', '$timeout', 'Sites', function ($scope, $timeout, sites) {
+        var helper = null, configIcon = null, localSites = [], dataModule;
+        helper = {
+            getDomain: function (url) {
+                var r = /:\/\/(.[^/]+)/;
+                return url.match(r)[1];
+            },
+            getLocalSites: function (callback) {
+                ntp.localData.getLocalSites(function (data) {
+                    if (!data.length) {//本地有数据
+                        ntp.localData.getTopSites(function (d) {
+                            d.length = d.length > 10 ? 10: d.length;
+                            for (var i = 0, j = d.length; i < j; i++) {
+                                var domain = helper.getDomain(d[i].url);
+                                d[i].icon = configIcon[domain] && configIcon[domain].icon || '';
+                                d[i].bgColor = configIcon[domain] && configIcon[domain].bgColor || '';
+                            }
+                            var localSites = [{
+                                "id": 1,
+                                "name": "Recommended",
+                                "items": d
+                            }];
+                            ntp.localData.setLocalSites(localSites);
+                            ///这里取服务器数据
+                            callback(localSites)
+                        });
+                    } else {
+                        console.log(data);
+                        callback(data);
+                    }
+                });
+            }
+        };
+        configIcon = {
+            "www.google.com": {"icon": "/resource/logo/google.svg", "bgColor": "#3369E8"},
+            "microsoft.com": { "bgColor": "#7CBB00"}
+        };
+
+        helper.getLocalSites(function (data) {
+            data = data[0].items;
+            localSites = data;
+            sites.query(function (data) {
+                for (var i = 0, j = data.length; i < j; i++) {
+                    var domain = helper.getDomain(data[i].url);
+                    data[i].letter = domain.substr(0, 2);
+                    localSites.push(data[i]);
+                }
+                $timeout(function () {
+                    $scope.items = localSites;
+                });
+            });
+//            $timeout(function () {
+//                $scope.items = localSites;
+//            });
         });
 
-    }]).directive('getTopSites', function () {
-
-    });
+    }]);
