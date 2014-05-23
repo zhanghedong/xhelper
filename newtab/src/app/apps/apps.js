20111c62e0ccfd67fcd721074130746c46b8683f
1625 <zhanghedong@gmail.com>
2014-05-23 17:33:42 +0800

    add apps module

--- newtab/src/app/apps/apps.js
+++ newtab/src/app/apps/apps.js
@@ -13,15 +13,12 @@ angular.module('apps', ['ngSanitize']).controller('appsCtrl', ['$scope', '$sce',
                     var i,j,item,items=[];
                     for(i = 0,j = data.length;i<j;i++){
                         item = data[i];
-                        if(item.isApp == 'true'){
+                        if(item.isApp == true){
                             item.icon = item.icons[item.icons.length - 1].url;
+                            items.push(item);
                         }
-                        items.push(item);
-
                     }
-                    console.log(items);
                     $scope.apps = items;
-
                 });
             });
         },
